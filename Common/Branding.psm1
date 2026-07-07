32bc26ea54ad79ec7e13cc16fd45dd3a9677c15e
BewustRon <166006529+BewustRon@users.noreply.github.com>
2026-07-07 15:17:43 +0200

    feat: update branding support URL and logo

--- Common/Branding.psm1
+++ Common/Branding.psm1
@@ -7,11 +7,11 @@ function Get-BICTCheckBranding {
         ProductName      = 'Bewust ICT Security Check'
         CompanyUrl       = 'https://www.bewustict.nl'
         SupportEmail     = 'noc@bewustict.nl'
-        SupportUrl       = 'https://www.bewustict.nl'
+        SupportUrl       = 'https://bewustict.nl/support/'
         PrivacyPolicyUrl = 'https://bewustict.nl/privacy_verklaring/'
         AboutUrl         = 'https://www.bewustict.nl'
         PrimaryColor     = '#63B1BC'
-        LogoUrl          = 'https://bewustict.nl/wp-content/uploads/2025/10/Logo-zonder-beeldberk.svg'
+        LogoUrl          = 'https://raw.githubusercontent.com/BewustRon/Check-Deployment/main/Assets/Bewust-ICT-beeldmerk-wit.svg'
     }
 }
 
